476e0188d2fa5f42d94a7f266145165bc4a6e6bd
chuck <121477964@qq.com>
2019-07-12 14:18:45 +0800

    新增了一个官网推荐的terser-webpack-plugin的webpack插件

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,14 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin'); // react官网推荐优化的包
 
 module.exports = merge(common, {
   devtool: 'source-map',
   mode: 'production',
+  optimization: {
+    minimizer: [new TerserPlugin({ /* additional options here */ })],
+  },
   plugins: [
     new UglifyJSPlugin({
       sourceMap: true,
